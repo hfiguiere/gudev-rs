09ed2bb41e4f224aa804ea3f4689b772938aec78
Hubert Figuière <hub@figuiere.net>
2016-09-05 20:51:53 -0400

    Fix compilation of regenerated files
    define callback_guard!() to no-op until gtk-rs/gir#271 is fixed

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
 
+extern crate libc;
 #[macro_use]
 extern crate glib;
+extern crate glib_sys as glib_ffi;
 extern crate gudev_sys as ffi;
+
+/// No-op
+macro_rules! callback_guard {
+    () => ()
+}
+
+pub use ffi::GUdevDeviceNumber as DeviceNumber;
 
 pub use auto::*;
 mod auto;
