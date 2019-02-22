1f8b3d070315f1fcdef32cb55c9884138d091759
Hubert Figuière <hub@figuiere.net>
2019-02-22 07:08:03 -0500

    gudev 0.6.0 and gudev-sys 0.5.0

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,6 @@ extern crate gobject_sys as gobject_ffi;
 extern crate glib_sys as glib_ffi;
 extern crate gudev_sys as ffi;
 
-/// No-op
-macro_rules! callback_guard {
-    () => ()
-}
-
 pub use ffi::GUdevDeviceNumber as DeviceNumber;
 
 pub use auto::*;
