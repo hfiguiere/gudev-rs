addb6ccfbce5164a3973d2f971d85cd95badf9be
Hubert Figuière <hub@figuiere.net>
2019-12-24 17:17:01 -0500

    gudev 0.8.0 for glib-rs 0.9.0

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,11 @@
 extern crate libc;
 #[macro_use]
 extern crate glib;
-extern crate gobject_sys as gobject_ffi;
-extern crate glib_sys as glib_ffi;
-extern crate gudev_sys as ffi;
+extern crate gobject_sys;
+extern crate glib_sys;
+extern crate gudev_sys;
 
-pub use ffi::GUdevDeviceNumber as DeviceNumber;
+pub use gudev_sys::GUdevDeviceNumber as DeviceNumber;
 
 pub use auto::*;
 mod auto;
