71916d875e074bdf14aec176aa81f02538b7fd45
Hubert Figuière <hub@figuiere.net>
2021-01-17 18:59:47 -0500

    Build with glib-rs unstable

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
 
 extern crate libc;
-#[macro_use]
 extern crate glib;
 extern crate gobject_sys;
 extern crate glib_sys;
-extern crate gudev_sys;
+extern crate gudev_sys as ffi;
 
 pub use gudev_sys::GUdevDeviceNumber as DeviceNumber;
 
