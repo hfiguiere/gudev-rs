5f6267c797c4040516ef466e7c4ff6212d5cb7b9
Bilal Elmoussaoui <belmouss@redhat.com>
2022-01-26 22:46:42 +0100

    Stop with the 2015ism and switch to 2021 edition

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,3 @@
-extern crate glib;
-extern crate glib_sys;
-extern crate gobject_sys;
-extern crate gudev_sys as ffi;
-extern crate libc;
-
 pub use ffi::GUdevDeviceNumber as DeviceNumber;
 
 pub use auto::*;
