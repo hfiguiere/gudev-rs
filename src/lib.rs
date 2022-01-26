16c8c69533c6160121841f18eff99e0553cc0cbb
Bilal Elmoussaoui <belmouss@redhat.com>
2022-01-26 22:43:49 +0100

    Regenerate with latest gir/gir-files

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
-
-extern crate libc;
 extern crate glib;
-extern crate gobject_sys;
 extern crate glib_sys;
+extern crate gobject_sys;
 extern crate gudev_sys as ffi;
+extern crate libc;
 
 pub use gudev_sys::GUdevDeviceNumber as DeviceNumber;
 
