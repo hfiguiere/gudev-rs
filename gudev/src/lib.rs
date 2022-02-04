348391fff220bd6b9f3a42074be487c3268cc80f
Bilal Elmoussaoui <belmouss@redhat.com>
2022-02-04 05:01:20 +0100

    Better exports (#4)
    * re-export prelude
    
    * re-export ffi
    
    * correctly rename bool getters

--- gudev/src/lib.rs
+++ gudev/src/lib.rs
@@ -1,6 +1,12 @@
+pub use ffi;
+
 pub type DeviceNumber = ffi::GUdevDeviceNumber;
 
 #[allow(unused_imports)]
 mod auto;
 
 pub use auto::*;
+
+pub mod prelude {
+    pub use super::auto::traits::*;
+}
