13f2d947c62c2ced8bc990f7d48d1b119532d176
Rene Harder <rehar@saweb.de>
2022-03-24 16:55:42 -0500

    Added examples and main lib documentation

---
+++ gudev/examples/monitor.rs
@@ -0,0 +1,30 @@
+use gudev::Device;
+use gudev::prelude::*;
+use gudev::Client;
+
+fn print_device(device: &Device, action: &str) {
+    println!(
+        "{}: {} {} (subsystem={}, sysname={}, devtype={})",
+        device.seqnum(),
+        action,
+        device.sysfs_path().unwrap_or_else(|| "---".into()),
+        device.subsystem().unwrap_or_else(|| "---".into()),
+        device.device_file().unwrap_or_else(|| "---".into()),
+        device.devtype().unwrap_or_else(|| "---".into()),
+    );
+}
+
+fn main() {
+
+    // Construct a Client object that can be used to query information about block devices
+    let client = Client::new(&["block"]);
+
+    // Connect to the client's uevent signal to monitor future udev events
+    client.connect_uevent(move |_, action, device| {
+        print_device(device, action)
+    });
+
+    // Create and start the glib main loop.
+    // Replace with gtk::main() in GTK+ application. 
+    glib::MainLoop::new(None, false).run();
+}
